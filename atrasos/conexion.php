6af7110ca12fc7637e80687760c442bf49fa06b4
Mario Sandoval <sandoval.mario.08@gmail.com>
2022-05-03 13:43:02 -0400

    Conexión a base de datos desde el Liceo

--- atrasos/conexion.php
+++ atrasos/conexion.php
@@ -1,46 +1,43 @@
 <?php
 
-echo "prueba";
 
-// function Conectarse(){
-// //$user = "adminpglvl";
-// //$password = "@dm1npgLVL";
-// //$dbname = "bdlvl";
-// $port = "5432";
-// $host = "localhost";
-// $user = "postgres";
-// $password = "";
-// $dbname = "test";
+// Antes de hacer la conexión, independiente del modo utilizado
+// Se debe configurar xampp para que acepte las conexiones a postgreSQL
 
-// $cadenaConexion = "host=$host port=$port dbname=$dbname user=$user password=$password";
+// respaldo función
+/*     
+    $user = "adminpglvl";
+    $password = "@dm1npgLVL";
+    $dbname = "bdlvl";
+    $port = "5432";
+    $host = "localhost";
+ */
 
-// $conexion = pg_connect($cadenaConexion) or die("Error en la Conexión: ".pg_last_error());
-// echo "<h3>Conexión Exitosa PHP - PostgreSQL</h3><hr><br>";
-// return $conexion;
-// }
+function Conectarse(){
 
+    $user = "postgres";
+    $password = "user2019";
+    $dbname = "dblvl_localhost";
+    $port = "5432";
+    $host = "localhost";
+  
 
-// function Conexion() {
-//     $host = "localhost";
-//     $dbname = "test";
-//     $username = "postgres";
-//     $password = "admin";
+    $cadenaConexion = "host=$host port=$port dbname=$dbname user=$user password=$password";
 
-//     try {
-//         $conn = new PDO("pgsql:host=$host; dbname=$dbname; $username; $password");
-//         echo "conexión exitosa";
-//     } catch (PDOException $e) {
-//         echo "La conexión fallo".$e;
-//     }
+    $conexion = pg_connect($cadenaConexion) or die("Error en la Conexión: ".pg_last_error());
+    //echo "<h3>Conexión Exitosa PHP - PostgreSQL</h3><hr><br>";
+    return $conexion;
+}
 
-// }
+Conectarse();
 
 
-function Conexion() {
-    $dns = 'pgsql:host=localhost; dbname=test';
+
+/* function Conexion() {
+    $dns = 'pgsql:host=localhost; dbname=dblvl_localhost';
 
     try {
-        $conexion_bd = new PDO($dns, 'postgres', 'admin');
+        $conexion_bd = new PDO($dns, 'postgres', 'user2019');
         $conexion_bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "conexion exitosa";
         return $conexion_bd;
@@ -50,7 +47,29 @@ function Conexion() {
     }
 }
 
-Conexion();
+Conexion(); */
+
+// phpinfo();
+
+
+
+// Conexión para probar en MAC ****************** 
+
+/* function Conexion() {
+    $dns = 'pgsql:host=localhost; dbname=dblvl_localhost';
+
+    try {
+        $conexion_bd = new PDO($dns, 'postgres', 'user2019');
+        $conexion_bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "conexion exitosa";
+        return $conexion_bd;
+
+    } catch (Exception $error) {
+        echo "ERROR: ".$error->getMessage();
+    }
+}
+
+Conexion(); */
 
 
 
