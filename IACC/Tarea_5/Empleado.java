0fac5a1a4f6426419e09314a974e4094b4c63538
M-sandoval-93 <sandoval.mario.08@gmail.com>
2022-05-03 23:47:10 -0400

    Agregar tarea IACC semana 5

---
+++ IACC/Tarea_5/Empleado.java
@@ -0,0 +1,20 @@
+/**
+ * Clase abstracta Empleado que hereda de persona
+ * 
+ * @author: Mario Sandoval Luengo
+ * Date: 04 de mayo 2022
+ */
+abstract class Empleado {
+    private String nombre;
+    private String apellidos;
+    private String DNI;
+    private String direccion;
+    private int anios_antiguedad;
+    private int telefono;
+    private double salario;
+    
+    
+    
+}
+
+
---
+++ IACC/Tarea_5/Persona.java
@@ -0,0 +1,13 @@
+/**
+ * Clase abstracta Persona
+ * @author: Mario Sandoval Luengo
+ * Date: 04 de mayo 2022
+ */
+
+abstract class Persona {
+    protected String nombre;
+    protected String apellidos;
+    protected String DNI;
+    protected String direccion;
+    
+}
