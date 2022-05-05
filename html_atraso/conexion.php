6001bd443b1b516fa53ec7e845acf09b8ae6f703
M-sandoval-93 <sandoval.mario.08@gmail.com>
2022-05-05 19:07:19 -0400

    Código listo para implementar, copiar datos relevantes

--- html_atraso/conexion.php
+++ html_atraso/conexion.php
@@ -1,10 +1,19 @@
 <?php
 function Conectarse(){
+    // conexión original servidor
 $user = "adminpglvl";
 $password = "@dm1npgLVL";
 $dbname = "bdlvl";
 $port = "5432";
-$host = "localhost";
+$host = "localhost"; 
+
+    // conexión local mac
+// $user = "postgres";
+// $password = "admin";
+// $dbname = "test";
+// $port = "5432";
+// $host = "localhost";
+
 
 $cadenaConexion = "host=$host port=$port dbname=$dbname user=$user password=$password";
 
