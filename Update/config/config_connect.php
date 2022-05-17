363f79af47eb947340a659891dc23cc6d3a4a892
M-sandoval-93 <sandoval.mario.08@gmail.com>
2022-05-16 20:38:22 -0400

    Actualizacion 160522_2038

---
+++ Update/config/config_connect.php
@@ -0,0 +1,11 @@
+<?php
+
+// Configuración para la conexión con la base de datos
+
+define ('DB_HOST', 'localhost');
+define ('DB_USSER', '');
+define ('DB_PASSWORD', '');
+define ('DB_DATA', '');
+define ('DB_CHARSET', 'utf8');
+
+?>
