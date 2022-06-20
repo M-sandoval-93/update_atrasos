44b83c5319556fd5eea7c5ee3773d132932b23f9
M-sandoval-93 <sandoval.mario.08@gmail.com>
2022-06-20 16:35:50 -0400

    Actualizacion 200622 16_35

--- Update/config/config_connect.php
+++ Update/config/config_connect.php
@@ -3,8 +3,9 @@
 // Configuración para la conexión con la base de datos
 define ('DB_HOST', 'localhost');
 define ('DB_USSER', 'postgres');
-define ('DB_PASSWORD', 'user2019');
-define ('DB_DATA', 'dblvl_localhost');
+// define ('DB_PASSWORD', 'user2019');
+define ('DB_PASSWORD', 'admin'); // clave del servidor que da acceso a la base de datos
+define ('DB_DATA', 'db_liceo');
 define ('DB_CHARSET', 'utf8');
 define ('DB_PORT', '5432');
 
---
+++ Update/views/login.php
@@ -0,0 +1,38 @@
+<?php
+
+include_once "models/connect.php";
+
+// $conexion = new Conexion();
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liceo Valentín Letelier</title>
+</head>
+<body>
+    <h1>prueba de contenido</h1>
+
+    <!-- agregar insignia del liceo con algun diseño y un escrito  -->
+
+<section>  
+    <!-- Agrgar el formulario de acceso
+    1.- pedirá usuario
+    2.- pedirá clave de usuario
+    3.- permitirá solicitar restauración de la clave o cambio de la misma
+    4.- tendrá link hacia:
+        - la página del liceo
+        - el facebook del liceo
+        - el instagram del liceo -->
+</section>
+
+<footer>
+    <!-- footer para mostrar información del liceo y del desarrollador del sitio -->
+</footer>
+    
+</body>
+</html>
