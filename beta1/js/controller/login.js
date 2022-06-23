ec75e98c9fce6e3bc20857b222d85f3dec0b59d6
M-sandova-93 <sandoval.mario.08@gmail.com>
2022-06-23 16:44:59 -0400

    actualizacion 230622

---
+++ beta1/js/controller/login.js
@@ -0,0 +1,4 @@
+$('#id_form_login').submit(function (e) {
+    e.preventDefault();
+    console.log("Prueba de contenido");
+});
--- beta1/views/login.php
+++ beta1/views/login.php
@@ -4,11 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/normalize.css">
     <title>Liceo Valentín Letelier</title>
 </head>
 <body>
+    <main class="design-login">
+        <div class="wave">
+            <img class="wave-img" src="assets/students.svg" alt="imagen fondo">
+        </div>
 
-<h1>prueba de contenido</h1>
-    
+        <div class="login">
+            <div class="login-data">
+                <img class="login-data-logo" src="assets/logo_liceo.png" alt="logo del liceo">
+            </div>
+        </div>
+    </main>
+
+
+    <script src="Pluggins/jQuery/jquery-3.6.0.min.js"></script>
+    <script src="js/controller/login.js"></script>    
 </body>
 </html>
