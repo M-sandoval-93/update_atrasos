4e3d4aa005cc03c95e5d03c2b89c524312d1f348
M-sandova-93 <sandoval.mario.08@gmail.com>
2022-06-24 12:59:54 -0400

    actualizacion 240622_1300

--- beta1/views/login.php
+++ beta1/views/login.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200;400;600;800&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/normalize.css">
     <title>Liceo Valentín Letelier</title>
@@ -16,7 +19,11 @@
 
         <div class="login">
             <div class="login-data">
-                <img class="login-data-logo" src="assets/logo_liceo.png" alt="logo del liceo">
+                <img class="login-data_logo" src="assets/logo_liceo.png" alt="logo del liceo">
+                <h1>Inisio Sesión</h1>
+                <form action="#" method="post" class="login-form">
+                    <input type="text" name="usuario" id="id_usuario">
+                </form>
             </div>
         </div>
     </main>
