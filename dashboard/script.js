8e45fae81628703d3fc57e1f69a2124739f1157b
M-sandova-93 <sandoval.mario.08@gmail.com>
2022-10-13 15:20:32 -0300

    actualizacion 131022_1520

---
+++ dashboard/script.js
@@ -0,0 +1,8 @@
+// let lista = document.querySelectorAll('.barra_navegacion li');
+
+// function activeLink() {
+//     lista.forEach((item) => item.classList.remove('hover_ed'));
+//     this.classList.add('hover_ed');  
+// }
+
+// lista.forEach((item) => item.addEventListener('mouseover', activeLink));
---
+++ post-movil/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="">
+    <title>prueba</title>
+</head>
+<body>
+
+    <h1>prueba de impresion</h1>
+
+    <form action="#" method="post">
+        <button id="btn_enviar">Enviar</button>
+    </form>
+
+    <!-- <script src="./plugin_impresion/plugin_impresora_termica.js"></script> -->
+    <script src="./print.min.js"></script>
+    <script src="./script.js"></script>
+    
+</body>
+</html>
